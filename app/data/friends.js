8c38035fc7ae4bb4a5774115bfed767be4c58794
olsoa4 <olsoa4@hotmail.com>
2018-10-06 22:40:30 -0500

    fin

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [{
+  name: 'Adam Olson',
+  profilePic: 'https://i.imgur.com/etssov1.jpg',
+  scores: [
+    "4",
+    "5",
+    "5",
+    "2",
+    "4",
+    "5",
+    "4",
+    "4",
+    "3",
+    "4"
+  ]
+}]
+
+module.exports = friendList;
---
+++ server.js
@@ -0,0 +1,26 @@
+//Dependencies:
+var express = require('express');
+var inquirer = require('inquirer');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(inquirer.json());
+app.use(inquirer.urlencoded({
+  extended: true
+}));
+app.use(inquirer.text());
+app.use(inquirer.json({
+  type: 'application/vnd.api+json'
+}));
+
+//ROUTER
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
